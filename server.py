dd4d21f43bf2f9e47394a6e7a3fd3da6c32e297d
mslycn <365775558@qq.com>
2026-01-22 15:41:02 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,31 @@
 
+import asyncio
+from wyoming.server import AsyncServer
+from wyoming.asr import AsrService
+from funasr import AutoModel
+
+MODEL_NAME = "paraformer-zh"
+
+class FunASRService(AsrService):
+    def __init__(self):
+        self.model = AutoModel(
+            model="paraformer-zh",
+            vad_model="fsmn-vad",
+            punc_model="ct-punc",
+            device="cpu"
+        )
+
+    async def transcribe(self, audio, sample_rate):
+        res = self.model.generate(
+            input=audio,
+            sample_rate=sample_rate
+        )
+        return res[0]["text"]
+
+async def main():
+    server = AsyncServer.from_uri("tcp://0.0.0.0:10300")
+    server.add_service(FunASRService())
+    await server.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
